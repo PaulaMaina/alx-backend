1cabfcf59dbd4c7b7004ea4c1e8f1d0543cbd59f
PaulaMaina <kabura.mainap@gmail.com>
2024-08-19 10:52:13 +0000

    Hypermedia del pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -42,7 +42,7 @@ class Server:
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Gets info about a page from a given index with a specified size"""
         data = self.indexed_dataset()
-        assert index ix not None and index >= 0 and index <= max(data.keys())
+        assert index is not None and index >= 0 and index <= max(data.keys())
         page_data = []
         count = 0
         next_idx = None
